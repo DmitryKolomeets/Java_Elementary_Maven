03af25f44682d578b9866ded2e3019f9e0abc077
Dmitry Kolomeets <bde@ukr.net>
2022-05-12 22:55:20 +0300

    added HappyNumber class

--- src/main/java/hometask14/HappyNumber.java
+++ src/main/java/hometask14/HappyNumber.java
@@ -1,6 +1,30 @@
 package hometask14;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class HappyNumber {
 
+    public boolean isHappy(int n) {
+
+        Set<Integer> unicSums = new HashSet<Integer>();
+        int squareSum;
+        int remain;
+        while (unicSums.add(n)) {
+            squareSum = 0;
+            while (n > 0) {
+                remain = n % 10;
+                squareSum += remain * remain;
+                n /= 10;
+            }
+            if (squareSum == 1)
+                return true;
+            else
+                n = squareSum;
+
+        }
+        return false;
     }
+
+
 }
