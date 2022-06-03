600c78f4a070e844e92c97664e4e5a0b8bfe9e5e
Dmitry Kolomeets <bde@ukr.net>
2022-06-03 16:07:43 +0300

    homework_15_warAndPeace init commit

---
+++ src/test/java/hometask15/WarAndPeaceTest.java
@@ -0,0 +1,13 @@
+package hometask15;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class WarAndPeaceTest {
+    WarAndPeace warAndPeace = new WarAndPeace();
+    @Test
+    public void warAndPeaceFilterAndCount() throws Exception{
+
+    }
+}
