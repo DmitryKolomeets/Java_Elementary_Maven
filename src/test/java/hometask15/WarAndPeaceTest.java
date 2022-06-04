0bd8eb8d1dc0ad7cbde744380aef5970601fa4ec
Dmitry Kolomeets <bde@ukr.net>
2022-06-04 19:43:51 +0300

    homework_15_warAndPeace init commit

--- src/test/java/hometask15/WarAndPeaceTest.java
+++ src/test/java/hometask15/WarAndPeaceTest.java
@@ -1,13 +1,47 @@
 package hometask15;
 
-import org.junit.Test;
+import org.junit.*;
+import org.junit.rules.TemporaryFolder;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 
 import static org.junit.Assert.*;
 
 public class WarAndPeaceTest {
     WarAndPeace warAndPeace = new WarAndPeace();
+    private static File dir;
+
+
+
+    @Rule
+    public TemporaryFolder folder= new TemporaryFolder();
+
+
+    @Test
+    public void testFileParser() throws IOException {
+        File createdFile= folder.newFile("warAndPeaceOutput.txt");
+        WarAndPeace warAndPeace1 = new WarAndPeace();
+       Path path = Paths.get("src/main/resources/vim.txt");
+        warAndPeace1.warAndPeaceFilterAndCount();
+        try (FileWriter fileWriter = new FileWriter(createdFile)) {
+            fileWriter.write("1.0 2.0 3.0 4.0");
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+
     @Test
     public void warAndPeaceFilterAndCount() throws Exception{
-
     }
+
+
+
+
 }
